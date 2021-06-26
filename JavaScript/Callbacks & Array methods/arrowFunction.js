a9ecf1e22adc910635219e4e8abbedc68a8a38c0
Daizo <69610587+DaizoWatanabe@users.noreply.github.com>
2021-06-26 17:36:26 -0300

    Add files via upload

---
+++ JavaScript/Callbacks & Array methods/arrowFunction.js
@@ -0,0 +1,8 @@
+/*
+Write an arrow function expression called greet. It should accept a single string argument, representing a person's name. It should return a greeting string as shown below:
+greet("Hagrid") //"Hey Hagrid!"
+*/
+
+const greet = (str) => {
+    return `Hey ${str}!`;
+};
