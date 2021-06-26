144d599a66ab0ff7c7e337bf5b75502679c1337c
Daizo <69610587+DaizoWatanabe@users.noreply.github.com>
2021-06-26 17:34:01 -0300

    Add files via upload

---
+++ JavaScript/Basics/ourFirstVariablesExercise.js
@@ -0,0 +1,6 @@
+/*Define a variable called myLuckyNumber and set it equal to 37.
+Define a variable called octopusLimbs and set it equal to 8*/
+
+
+const myLuckyNumber = 37;
+const octopusLimbs = 8;
