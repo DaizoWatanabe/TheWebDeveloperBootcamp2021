67c311e71a2e07f393c48432b2f42214c3e11f64
DaizoWatanabe <daizowatanabe@gmail.com>
2022-02-27 12:15:21 -0300

    add yelpcamp public and routes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,12 +3,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-//const Joi = require("joi");
-const { campgroundSchema, reviewSchema } = require("./schemas");
 const ExpressError = require("./utils/ExpressError");
-const catchAsync = require("./utils/catchAsync");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
+const session = require("express-session");
+const flash = require("connect-flash");
+//const Joi = require("joi");
+
+const campgroundsRouter = require("./routes/campgrounds");
+const reviewsRouter = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -22,6 +23,12 @@ db.once("open", () => {
   console.log("Connection established");
 });
 
+const sessionConfig = {
+  secret: "yelpcampsecret",
+  resave: false,
+  saveUninitialized: false,
+};
+
 const app = express();
 
 app.engine("ejs", ejsMate);
@@ -30,141 +37,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sessionConfig));
+app.use(flash());
 
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-}
+app.use("/campgrounds", campgroundsRouter);
+app.use("/campgrounds/:id/reviews", reviewsRouter);
 
 app.get("/", (req, res) => {
   res.render("campgrounds/home");
 });
-
-app.get(
-  "/campgrounds",
-  catchAsync(async (req, res, next) => {
-    const campgrounds = await Campground.find({});
-    if (!campgrounds) {
-      next(new ExpressError("Campgrounds not found", 404));
-    }
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
-
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
-
-app.post(
-  "/campgrounds",
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const { title, city, state, description, price, image } =
-      req.body.campground;
-    const location = `${city}, ${state}`;
-    const campground = new Campground({
-      title,
-      location,
-      description,
-      price,
-      image,
-    });
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.get(
-  "/campgrounds/:id",
-  catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    if (!campground) {
-      next(new ExpressError("Campground not found", 404));
-    }
-    res.render("campgrounds/show", { campground });
-  })
-);
-
-app.get(
-  "/campgrounds/:id/edit",
-  catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    if (!campground) {
-      next(new ExpressError("Campground not found", 404));
-    }
-    const array = campground.location.split(","); //since I used 2 fields for location (city and state) I had to trim
-    const city = array[0];
-    const state = array[1];
-    res.render("campgrounds/edit", { campground, city, state });
-  })
-);
-
-app.put(
-  "/campgrounds/:id",
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const { city, state } = req.body.campground;
-    const location = `${city}, ${state}`;
-    const campground = await Campground.findByIdAndUpdate(id, {
-      ...req.body.campground,
-      location,
-    });
-    if (!campground) {
-      next(new ExpressError("Campground not found", 404));
-    }
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.delete(
-  "/campgrounds/:id",
-  catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-  })
-);
-
-app.post(
-  "/campgrounds/:id/reviews",
-  validateReview,
-  catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async (req, res, next) => {
-  //res.send('made it to the review deletion path')
-  //const campground = await Campground.findById(req.params.id);
-  //const review = await Review.findById(req.params.reviewId)
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-  await Review.findByIdAndDelete(reviewId)
-  res.redirect(`/campgrounds/${id}`)
-}))
 
 //anything outside of what was mapped will fall into this
 app.all("*", (req, res, next) => {
---
+++ YelpCamp/routes/campgrounds.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const router = express.Router();
+
+const Campground = require("../models/campground");
+
+const { campgroundSchema } = require("../schemas");
+
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+router.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({});
+    if (!campgrounds) {
+      next(new ExpressError("Campgrounds not found", 404));
+    }
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const { title, city, state, description, price, image } =
+      req.body.campground;
+    const location = `${city}, ${state}`;
+    const campground = new Campground({
+      title,
+      location,
+      description,
+      price,
+      image,
+    });
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    if (!campground) {
+      next(new ExpressError("Campground not found", 404));
+    }
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      next(new ExpressError("Campground not found", 404));
+    }
+    const array = campground.location.split(","); //since I used 2 fields for location (city and state) I had to trim
+    const city = array[0];
+    const state = array[1];
+    res.render("campgrounds/edit", { campground, city, state });
+  })
+);
+
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { city, state } = req.body.campground;
+    const location = `${city}, ${state}`;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+      location,
+    });
+    if (!campground) {
+      next(new ExpressError("Campground not found", 404));
+    }
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
