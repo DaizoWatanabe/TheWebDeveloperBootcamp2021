b4d76118dae81e3170b413e6596c7a863a5860bf
DaizoWatanabe <daizowatanabe@gmail.com>
2022-03-21 20:35:26 -0300

    add returnTo behavior after login

--- YelpCamp/routes/user.js
+++ YelpCamp/routes/user.js
@@ -36,7 +36,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'Welcome Back!');
-  res.redirect('/campgrounds');
+  const redirectUrl = req.session.returnTo || '/campgrounds';
+  res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res) => {
