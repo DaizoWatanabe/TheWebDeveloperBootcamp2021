152a62eba03297bc732a4e5dc0ca0a97f0f02aef
DaizoWatanabe <daizowatanabe@gmail.com>
2022-03-27 20:25:42 -0300

    add start review

--- YelpCamp/routes/user.js
+++ YelpCamp/routes/user.js
@@ -10,13 +10,13 @@ const users = require('../controllers/users');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+  .get(users.renderLogin)
+  .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout);
 
